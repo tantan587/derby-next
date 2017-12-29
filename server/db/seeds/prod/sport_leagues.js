273c6129dbc5c956db62d12a1ac6940893ef61c1
tantan587 <yoni.h.silverman@gmail.com>
2017-12-28 21:25:14 -0600

    fixed error in seed file

--- server/db/seeds/prod/sport_leagues.js
+++ server/db/seeds/prod/sport_leagues.js
@@ -6,7 +6,7 @@ var data =
   {sport_name:'NHL',sport_id:104,type:'Hockey',fantasy_data_key:'TBD'},
   {sport_name:'CFB',sport_id:105,type:'Football',fantasy_data_key:'TBD'},
   {sport_name:'CBB',sport_id:106,type:'Basketball',fantasy_data_key:'TBD'},
-  {sport_name:'EPL',sport_id:107,type:'Soccer',fantasy_data_key:'TBD'},
+  {sport_name:'EPL',sport_id:107,type:'Soccer',fantasy_data_key:'TBD'}]
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
