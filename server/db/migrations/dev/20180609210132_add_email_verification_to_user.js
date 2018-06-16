0c525a4f836eff8cd2673acaba95739bdcbedd7d
Gabe Trinidadd <gabe.trinidad@vulcansearch.com>
2018-06-16 18:55:42 -0500

    Made email verification fields non nullable

--- server/db/migrations/dev/20180609210132_add_email_verification_to_user.js
+++ server/db/migrations/dev/20180609210132_add_email_verification_to_user.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.withSchema('users').table('users', (t) => {
-    t.boolean('verified')
-    t.string('verification_code')
+    t.boolean('verified').notNullable().defaultTo(false)
+    t.string('verification_code').notNullable().defaultTo(Math.floor(1000 + Math.random() * 9000))
     t.timestamp('expires_at').notNullable().defaultTo(knex.raw("now() + INTERVAL '15 minutes'"))
   })
 };
