93c03346032ae17ba483e31b57206b0a507bc23c
camila8 <camilabenaim2012@u.northwestern.edu>
2018-06-17 17:24:30 -0500

    added EPL to stadium
    only UK soccer stadiums

--- scripts/createSports/create_stadiums.js
+++ scripts/createSports/create_stadiums.js
@@ -16,8 +16,9 @@ const createAllStadiums = async () => {
   let NHL_stadiums = await getStadiumInfo(knex, 'NHL', 'NHLv3ScoresClient', 'getStadiumsPromise')
   let NFL_stadiums = await getStadiumInfo(knex, 'NFL', 'NFLv3ScoresClient', 'getStadiumsPromise')
   let CFB_stadiums = await getStadiumInfo(knex, 'CFB', 'CFBv3ScoresClient', 'getStadiumsPromise')
-
-  let data = MLB_stadiums.concat(NBA_stadiums).concat(NHL_stadiums).concat(NFL_stadiums).concat(CFB_stadiums)
+  let EPL_stadiums = await getStadiumInfo(knex, 'EPL', 'Soccerv3ScoresClient','getVenuesPromise')
+  console.log(EPL_stadiums[0])
+  let data = MLB_stadiums.concat(NBA_stadiums).concat(NHL_stadiums).concat(NFL_stadiums).concat(CFB_stadiums).concat(EPL_stadiums)
   db_helpers.insertIntoTable(knex, 'sports', 'stadium', data)
     .then(()=>{
       // sport id+ 100+ stadium id
@@ -35,19 +36,22 @@ const getStadiumInfo = async (knex, sportName, api, promiseToGet) => {
   // console.log(stadiumData)
   // let sport_id = resp[1]
   // console.log(sport_id)
+  let UK = ["England", "Wales", "Scotland", "Northern Ireland"]
   let stadiumData = await db_helpers.getFdata (knex, sportName, api, promiseToGet)
   let sportid = await db_helpers.getSportId(knex,sportName)
 /*   .then((FantasyDataClient) => {
     FantasyDataClient.MLBv3ScoresClient.getStadiumsPromise() */
   let cleanStadiums = JSON.parse(stadiumData)
-
-  stadiumInfo = cleanStadiums.map(stadium =>
+  let stadiums = sportid[0].sport_id === '107' ? cleanStadiums.filter(stadium => UK.includes(stadium.Country)) : cleanStadiums
+  console.log('sportid', sportid[0].sport_id, 'stadium length:', stadiums.length)
+  console.log(stadiums[0])
+  let s = sportid[0].sport_id
+  stadiumInfo = stadiums.map(stadium =>
     {
-
-      let s = sportid[0].sport_id
-      let i = stadium.StadiumID
+      let i = s === '107' ? stadium.VenueId : stadium.StadiumID
+      let state = s === '107' ? stadium.Country : stadium.State
       const StadiumID = s * 1000 + i + 100
-      return {stadium_id: StadiumID, name: stadium.Name, city: stadium.City, state: stadium.State}
+      return {stadium_id: StadiumID, name: stadium.Name, city: stadium.City, state: state}
   }
   )
   //console.log('here')
