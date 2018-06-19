856f3adbc8d495d8e7bf19eedf715c3acf3ea765
Jeremy Odell <jeremy0dell@yahoo.com>
2018-06-18 21:36:56 -0500

    begin team settings

---
+++ common/components/TeamSettings/TeamSettings.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+class TeamSettings extends React.Component {
+
+  render() {
+    return (  
+      <div>
+        hello again
+      </div>
+    )
+  }
+}
+
+export default withStyles()(TeamSettings)
---
+++ pages/mainleaguesettings.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import LayoutLeague from '../common/components/LayoutLeague'
+import withRoot from '../common/components/withRoot'
+import withRedux from 'next-redux-wrapper'
+import storeFactory from '../common/store'
+import MainLeagueSettings from '../common/components/MainLeague/MainLeagueSettings'
+import RouteProtector from '../common/components/RouteProtector'
+
+
+class Standings extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <LayoutLeague >
+          <RouteProtector
+            ProtectedRoute={() =>
+              <MainLeagueSettings />}
+            previousPage={'mainleaguesettings'}/>
+        </LayoutLeague>
+      </div>
+    )
+  }
+}
+export default withRedux(storeFactory)(withRoot(Standings))
