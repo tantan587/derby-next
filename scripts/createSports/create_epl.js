aee8f9ce73b22771078a2388b3f92a5dbcc7f462
Maltman <32251012+matherts@users.noreply.github.com>
2018-07-04 23:48:11 -0500

    modified function to pull from BOTH epl seasons
    this means both last year, and this years season

--- scripts/createSports/create_epl.js
+++ scripts/createSports/create_epl.js
@@ -3,5 +3,7 @@ const createSport = require('./create_sport_helpers')
 //const db_helpers = require('../helpers').data
 const knex = require('../../server/db/connection')
 
-createSport.createSoccerLeague(knex, '107', 'EPL', 'Soccerv3StatsClient', 'getCompetitionFixturesLeagueDetailsPromise', 1)
+createSport.createSoccerLeague(knex, '107', 'EPL', 'Soccerv3StatsClient', 'getSeasonTeamsPromise', 64, 37)
 
+//2018 season id: 37
+//2019 season id: 64
--- scripts/createSports/create_sport_helpers.js
+++ scripts/createSports/create_sport_helpers.js
@@ -83,31 +83,58 @@ const createProfessionalSport = async (knex, sport_id, sportName, api, promiseTo
         }) 
 }
 
-const createSoccerLeague = async (knex, sport_id, sportName, api, promiseToGet, competition_id) => {
+const createSoccerLeague = async (knex, sport_id, sportName, api, promiseToGet, season_id_1, season_id_2) => {
+    //need to pull in data from 2 seasons, since teams get promoted and relegated.
+    //upcoming season is season 1 - currently, 2018/19
+    //last season is season 2 - 2017/18
     let teamInfo = []
     let standings = []
     let playoff_standings = []
-    let teams_and_info = await db_helpers.createSportData(knex, sport_id, sportName, api, promiseToGet, competition_id)
+    let teams_and_info = await db_helpers.createSportData(knex, sport_id, sportName, api, promiseToGet, season_id_1)
+    let teams_2 = await db_helpers.getFdata(knex, sportName, api, promiseToGet, season_id_2)
+    let soccer_teams_2 = JSON.parse(teams_2)
     let teamIdMap = teams_and_info[1]
     let soccer_teams = teams_and_info[0]
     let confMap = teams_and_info[2]
+    let season_1_team_ids = []
+    let premier_table = []
+    let team_conference = confMap[sportName]
 
-    soccer_teams.Teams.forEach(team => {
-        console.log(team.GlobalTeamId)
-        let team_id = teamIdMap[team.GlobalTeamId]
-        let team_conference = confMap[sportName]
+    soccer_teams.forEach(team => {
+        let team_id = teamIdMap[team.Team.GlobalTeamId]
         console.log(team_id)
+        season_1_team_ids.push(team_id)
         
-        teamInfo.push({sport_id: sport_id, team_id: team_id, key: team.Key, city: team.City, 
-            name: team.Name, conference_id: team_conference, 
-            logo_url: team.WikipediaLogoUrl ? team.WikipediaLogoUrl: "---",
-            global_team_id: team.GlobalTeamId})
+        teamInfo.push({sport_id: sport_id, team_id: team_id, key: team.Team.Key, city: team.Team.City, 
+            name: team.Team.Name, conference_id: team_conference, 
+            logo_url: team.Team.WikipediaLogoUrl ? team.Team.WikipediaLogoUrl: "---",
+            global_team_id: team.Team.GlobalTeamId})
 
         standings.push({team_id: team_id, wins : 0, losses: 0, ties: 0})
         
         playoff_standings.push({team_id: team_id, playoff_wins : 0, playoff_losses: 0, byes: 0, bowl_wins: 0, playoff_status: 'tbd'})
+
+        premier_table.push({team_id: team_id, division_1: true})
     
     })
+
+    soccer_teams_2.forEach(team=>{
+        let team_id = teamIdMap[team.Team.GlobalTeamId]
+        if(!season_1_team_ids.includes(team_id)){
+            teamInfo.push({sport_id: sport_id, team_id: team_id, key: team.Team.Key, city: team.Team.City, 
+                name: team.Team.Name, conference_id: team_conference, 
+                logo_url: team.Team.WikipediaLogoUrl ? team.Team.WikipediaLogoUrl: "---",
+                global_team_id: team.Team.GlobalTeamId})
+    
+            standings.push({team_id: team_id, wins : 0, losses: 0, ties: 0})
+            
+            playoff_standings.push({team_id: team_id, playoff_wins : 0, playoff_losses: 0, byes: 0, bowl_wins: 0, playoff_status: 'tbd'})
+
+            premier_table.push({team_id: team_id, division_1: false})
+        }
+    })
+
+
 
 
 
@@ -119,8 +146,12 @@ const createSoccerLeague = async (knex, sport_id, sportName, api, promiseToGet, 
             {
             db_helpers.insertIntoTable(knex, 'sports', 'playoff_standings', playoff_standings)
             .then(()=> {
-                process.exit()
-            })
+                db_helpers.insertIntoTable(knex, 'sports', 'premier_status', premier_table)
+                .then(()=>{
+                    process.exit()
+                    })
+
+                })
             })
         }) 
 }
