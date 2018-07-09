03e8746dada21e3906cad41a4662c14f83aa7b0a
Maltman <32251012+matherts@users.noreply.github.com>
2018-07-09 18:43:07 -0400

    update Schedule migration

--- server/db/migrations/dev/20180706083341_schedule_table_update.js
+++ server/db/migrations/dev/20180706083341_schedule_table_update.js
@@ -2,15 +2,21 @@
 exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema.withSchema('sports').table('schedule', function(t) {
-            t.integer('home_team_score',6,0).notNullable()
-            t.integer('away_team_score',6,0).notNullable()
-            t.string('status').notNullable()
-            t.string('winner').notNullable()
-            t.string('time').notNullable()
-            t.string('period').notNullable()
-            t.string('updated_time').notNullable()
-            t.integer('season_type', 2,0).notNullable()
-            t.integer('year',4,0).notNullable()
+            t.integer('home_team_score',6,0)
+            t.integer('away_team_score',6,0)
+            t.string('status')
+            t.string('winner')
+            t.string('time')
+            t.string('period')
+            t.string('updated_time')
+            t.integer('season_type', 2,0)
+            t.integer('year',4,0)
+        }),
+        knex.schema.withSchema('sports').table('standings', function(t) {
+            t.integer('year',4,0)
+        }),
+        knex.schema.withSchema('sports').table('playoff_standings', function(t) {
+            t.integer('year',4,0)
         })
     ])     
 };
@@ -26,6 +32,13 @@ exports.down = function(knex, Promise) {
             table.dropColumn('period')
             table.dropColumn('updated_time')
             table.dropColumn('season_type')
-    })
-  ])
+            table.dropColumn('year')
+    }),
+        knex.schema.withSchema('sports').table('standings', (table) => {
+            table.dropColumn('year')
+    }),
+        knex.schema.withSchema('sports').table('playoff_standings', (table) => {
+            table.dropColumn('year')
+})
+    ])
 };
