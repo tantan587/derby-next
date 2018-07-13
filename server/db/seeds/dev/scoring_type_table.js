133bca3bbc7e826ca34eec2499b3c580c068c0a7
Yoni Silverman <yoni.silverman@mcmaster.com>
2018-07-13 14:10:36 -0700

    creating default point structure as seed

---
+++ server/db/seeds/dev/scoring_type_table.js
@@ -0,0 +1,10 @@
+var data = require('../../../../data/default_point_structure.json')
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return  knex.withSchema('fantasy').table('scoring').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex.withSchema('fantasy').table('scoring').insert(data)
+    })
+}
