2c9f6159a0d13574d151ea2b5c0f2e23b672153c
Maltman <matherts@gmail.com>
2018-07-20 13:44:18 -0500

    more changes

--- scripts/updateStandings.js
+++ scripts/updateStandings.js
@@ -1,7 +1,7 @@
 const db_helpers = require('./helpers').data
 const fantasyHelpers = require('../server/routes/helpers/fantasyHelpers')
 const knex = require('../server/db/connection')
-const getDayCount = require('./Analysis/dayCount.js')
+//const getDayCount = require('./Analysis/dayCount.js')
 
 //note: college basketball has an extra, old functino below: waiting to be sure we don't need this again
 
@@ -18,24 +18,21 @@ async function updateStandings()
   let eplData = await standingsBySport(knex, 'EPL', 'Soccerv3StatsClient', 'getStandingsPromise', '144')
   let data = nhlData.concat(nbaData).concat(mlbData).concat(nflData).concat(cfbData).concat(eplData).concat(cbbData)
 
-  db_helpers.updateStandings(knex, data)
-    .then(result => {
-      console.log('Number of Standings Updated: ' + result)
-      //we will need to figure out if below needs to be added in, where this function updates fantasy points. Unclear, can't remember off top of head
-      fantasyHelpers.updatePoints()
-        .then(() =>{
-          process.exit()
-        })
-      
-    })
+  let result =  await db_helpers.updateStandings(knex, data)
+  console.log('Number of Standings Updated: ' + result)
+  await fantasyHelpers.updateTeamPoints()
+  await fantasyHelpers.updateLeaguePoints()
+  console.log('im done')
+  process.exit()
+
 }
 
 const standingsBySport = async (knex, sportName, api, promiseToGet, year) => {
   let standings_info = await db_helpers.createStandingsData(knex, sportName, api, promiseToGet, year)
   let newStandings = standings_info.map(team=>{
     let ties = sportName === 'NHL' ? team.OvertimeLosses : 
-              sportName === 'NFL' ? team.Ties : 
-              sportName === 'EPL' ? team.Draws : 0
+      sportName === 'NFL' ? team.Ties : 
+        sportName === 'EPL' ? team.Draws : 0
 
     return {team_id: team.team_id, wins: team.Wins, losses: team.Losses, ties: ties, year: team.Season}
   })
@@ -49,11 +46,11 @@ const getCFBstandings = async (knex, sportName, api, promiseToGet, year) =>{
   //This could also be done with dates and season status table, as had been discussed
   //table hasn't yet been created
   let current_week = await db_helpers.getFdata(knex, sportName, api, 'getCurrentWeekPromise')
-  if(current_week !== "" && current_week < 16){
+  if(current_week !== '' && current_week < 16){
     return standings
   }else{
     //pull, and then make readable, playoff games (including bowl games)
-    let post_year = year.concat("POST")
+    let post_year = year.concat('POST')
     let non_parse_games = await db_helpers.getFdata(knex, sportName, api, 'getGamesByWeekPromise', post_year, 1)
     let playoff_games = JSON.parse(non_parse_games)
 
@@ -124,52 +121,52 @@ const getCFBstandings = async (knex, sportName, api, promiseToGet, year) =>{
     let new_bowl_wins = bowl_wins.filter(team => finalist_team_ids.includes(team.team_id)===false)
     let new_standings = Object.keys(standings_by_team_id).map(key => standings_by_team_id[key])
     
-    let playoff_updated = await db_helpers.updateBowlWins(knex, new_bowl_wins, new_playoff_teams)
+    await db_helpers.updateBowlWins(knex, new_bowl_wins, new_playoff_teams)
 
     return new_standings
   }
 }
 
-const getCBBstandings = async (knex, sportName, api, promiseToGet, year) =>{
-  const standings = await standingsBySport(knex, sportName, api, promiseToGet, year)
-  var today = new Date()
-  let today_day_count = getDayCount(today)
-  console.log(today_day_count)
+// const getCBBstandings = async (knex, sportName, api, promiseToGet, year) =>{
+//   const standings = await standingsBySport(knex, sportName, api, promiseToGet, year)
+//   var today = new Date()
+//   let today_day_count = getDayCount(today)
+//   console.log(today_day_count)
 
-  //note: below is an approximation of date count of start of post season
-  //this should be pulled from table, with start dates
-  //we can create this table using get current season, which is a pull from fantasy data
-  //will want to create that
-  if(today_day_count<1667){
-    return standings
-  }else{
-    //pull ALL postseason games (includes NIT)
-    const post_year = year.concat("POST")
-    const unparsed_post_season_games = await db_helpers.getFdata(knex, sportName, api, "getSchedulesPromise", post_year)
-    const post_season_games = JSON.parse(unparsed_post_season_games)
-    let teamIdMap = await db_helpers.getTeamIdMap(knex, '106')
-    //break standings up by teamID, into object
-    let standings_by_team_id = {}
-    standings.forEach(team => standings_by_team_id[team.team_id]={...team})
-    //for each postseason game: take results away from standings
-    post_season_games.forEach(game =>{
+//   //note: below is an approximation of date count of start of post season
+//   //this should be pulled from table, with start dates
+//   //we can create this table using get current season, which is a pull from fantasy data
+//   //will want to create that
+//   if(today_day_count<1667){
+//     return standings
+//   }else{
+//     //pull ALL postseason games (includes NIT)
+//     const post_year = year.concat('POST')
+//     const unparsed_post_season_games = await db_helpers.getFdata(knex, sportName, api, 'getSchedulesPromise', post_year)
+//     const post_season_games = JSON.parse(unparsed_post_season_games)
+//     let teamIdMap = await db_helpers.getTeamIdMap(knex, '106')
+//     //break standings up by teamID, into object
+//     let standings_by_team_id = {}
+//     standings.forEach(team => standings_by_team_id[team.team_id]={...team})
+//     //for each postseason game: take results away from standings
+//     post_season_games.forEach(game =>{
 
-      let results = game.HomeTeamScore > game.AwayTeamScore ? [teamIdMap[game.GlobalHomeTeamID],teamIdMap[game.GlobalAwayTeamID]] : [teamIdMap[game.GlobalAwayTeamID],teamIdMap[game.GlobalHomeTeamID]]
-      standings_by_team_id[results[0]].wins--
-      standings_by_team_id[results[1]].losses--
-    })
-    //put standings back together into object
-    let new_standings = Object.keys(standings_by_team_id).map(key => standings_by_team_id[key])
-    return new_standings
-  }
-}
+//       let results = game.HomeTeamScore > game.AwayTeamScore ? [teamIdMap[game.GlobalHomeTeamID],teamIdMap[game.GlobalAwayTeamID]] : [teamIdMap[game.GlobalAwayTeamID],teamIdMap[game.GlobalHomeTeamID]]
+//       standings_by_team_id[results[0]].wins--
+//       standings_by_team_id[results[1]].losses--
+//     })
+//     //put standings back together into object
+//     let new_standings = Object.keys(standings_by_team_id).map(key => standings_by_team_id[key])
+//     return new_standings
+//   }
+// }
 
 //this I'm saving for later, to keep track of for EPL when we need it.
 //this is the round for each year, when we end up only using the year that is needed.
-const round_year = {
-  2018: 144,
-  2019: 274
-}
+// const round_year = {
+//   2018: 144,
+//   2019: 274
+// }
 
 
 updateStandings()
