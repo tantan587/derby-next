6b496037f88e00c4be9bab03f7ffdcd84f54d2a1
Yoni Silverman <yoni.silverman@mcmaster.com>
2018-08-18 19:32:56 -0500

    seeing if i can get the scheduler to run

--- server/worker.js
+++ server/worker.js
@@ -1,1 +1,18 @@
-setInterval(() => console.log('helloWorker'), 5000)
+const script1 = require('./updatePlayoffsSchedule')
+const script2 = require('./updateSchedule')
+const script3 = require('./updatePlayoffStandings')
+const script4 = require('./updateStandings')
+const script5 = require('./updateDayOfSchedule')
+
+
+const everyMinute = async () => {
+  await script1.createSchedule(false)
+  await script2.createSchedule(false)
+  await script3.createStandingsPO(false)
+  await script4.updateStandings(false)
+  await script5.updateBoxScoreJSON(true, 'near')
+}
+
+setInterval(() => everyMinute(), 60000)
+
+
