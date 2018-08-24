b8aaee14e0cc5bc159e09ad47a4264a2c0c4e36e
Josh Bay <joshmbay1@gmail.com>
2018-08-24 15:59:20 -0500

    removed dark green bar on nav

--- common/components/Navigation/TopNavUser.js
+++ common/components/Navigation/TopNavUser.js
@@ -1,7 +1,7 @@
 const R = require('ramda')
 import React from 'react'
-import {connect} from 'react-redux'
-import {withStyles} from '@material-ui/core/styles'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
@@ -9,12 +9,12 @@ import HamburgerIcon from '@material-ui/icons/Reorder'
 
 import MenuButton from './Buttons/MenuButton'
 import LeaguesButton from './Buttons/LeaguesButton'
-import {toggleMobileNav, setMobileNavVariant} from '../../actions/status-actions'
+import { toggleMobileNav, setMobileNavVariant } from '../../actions/status-actions'
 
 const styles = (theme) => ({
   root: {
     flexGrow: 1,
-    backgroundColor:'white'
+    backgroundColor: 'white'
   },
   toolbar: {
     display: 'flex',
@@ -33,9 +33,9 @@ const styles = (theme) => ({
     marginRight: 20,
     float: 'right'
   },
-  toolbarHeight :{
-    height:30,
-    maxHeight:30,
+  toolbarHeight: {
+    height: 30,
+    maxHeight: 30,
   },
   hamburger: {
     position: 'absolute',
@@ -51,50 +51,47 @@ class TopNavUser extends React.Component {
     this.props.setMobileNavVariant('TopNavHomeVariant')
   }
   state = {
-    hoverIndex:-1
+    hoverIndex: -1
   };
 
 
-  setHover = (hoverIndex) =>
-  {
-    this.setState({hoverIndex:hoverIndex})
+  setHover = (hoverIndex) => {
+    this.setState({ hoverIndex: hoverIndex })
   }
- 
-  setHoverToButton = (buttonType, index, loggedIn, link, name) =>
-  {
-    let hoverColor = this.state.hoverIndex===index?'#EBAB38':'white'
+
+  setHoverToButton = (buttonType, index, loggedIn, link, name) => {
+    let hoverColor = this.state.hoverIndex === index ? '#EBAB38' : 'white'
     return <div
-      onMouseEnter={() => this.setHover(index)} 
-      onMouseLeave={() => this.setHover(-1)} 
-      style={{display:'inline-flex'}}>
-      {buttonType == 'league'  
-        ? loggedIn ? <LeaguesButton name='My Leagues' color={hoverColor} /> : <div/> 
-        : buttonType == 'home' 
-          ? <MenuButton color={hoverColor} link={link} name={name} isHomeLogo={true}/>
-          : <MenuButton color={hoverColor} link={link} name={name}/>
+      onMouseEnter={() => this.setHover(index)}
+      onMouseLeave={() => this.setHover(-1)}
+      style={{ display: 'inline-flex' }}>
+      {buttonType == 'league'
+        ? loggedIn ? <LeaguesButton name='My Leagues' color={hoverColor} /> : <div />
+        : buttonType == 'home'
+          ? <MenuButton color={hoverColor} link={link} name={name} isHomeLogo={true} />
+          : <MenuButton color={hoverColor} link={link} name={name} />
       }
-    </div> 
+    </div>
   }
 
   render() {
-    const {classes, user, toggleMobileNav} = this.props
+    const { classes, user, toggleMobileNav } = this.props
     return (
       <div className={classes.root}>
-        <AppBar position="static" style={{backgroundColor:'229246', color:'white'}}>
-          <div style={{backgroundColor:'#00642C', height:30}}/>
+        <AppBar position="static" style={{ backgroundColor: '229246', color: 'white' }}>
           <Toolbar className={classes.toolbar}>
-            <Button className={classes.hamburger}  onClick={toggleMobileNav}>
+            <Button className={classes.hamburger} onClick={toggleMobileNav}>
               <HamburgerIcon />
             </Button>
-            
-            {this.setHoverToButton('home', 0, user.loggedIn, '/')} 
+
+            {this.setHoverToButton('home', 0, user.loggedIn, '/')}
             <div className={classes.flex}>
-              {this.setHoverToButton('league', 1, user.loggedIn)}  
+              {this.setHoverToButton('league', 1, user.loggedIn)}
               {this.setHoverToButton('default', 2, user.loggedIn, '/participate', 'Create/Join League')}
               {this.setHoverToButton('default', 3, user.loggedIn, '/rules', 'Rules')}
               {this.setHoverToButton('default', 4, user.loggedIn, '/faq', 'FAQ')}
               {this.setHoverToButton('default', 5, user.loggedIn, '/scoreboard', 'Scoreboard')}
-              <div style={{float:'right'}}>
+              <div style={{ float: 'right' }}>
                 {user.loggedIn ?
                   this.setHoverToButton('default', 6, user.loggedIn, '/logout', 'Log out')
                   :
@@ -114,5 +111,5 @@ class TopNavUser extends React.Component {
 
 export default R.compose(
   withStyles(styles),
-  connect(R.pick(['user']), {toggleMobileNav: toggleMobileNav('TopNavHomeVariant'), setMobileNavVariant}),
+  connect(R.pick(['user']), { toggleMobileNav: toggleMobileNav('TopNavHomeVariant'), setMobileNavVariant }),
 )(TopNavUser)
