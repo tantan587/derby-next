03450ca41745dc59a60789437cbe3d74e895d4f0
Josh Bay <joshmbay1@gmail.com>
2018-09-02 12:44:19 -0500

    unfinished team/roster filter added to scoreboard

--- common/components/Scoreboard/ScoreboardPage.js
+++ common/components/Scoreboard/ScoreboardPage.js
@@ -102,32 +102,51 @@ class ScoreboardPage extends React.Component {
   render() {
     //const { classes, liveGames} = this.props
 
-    const {contentFilter} = this.props
-    const {mySchedule, date} = this.state
-    const page='scoreboard'
+    const { contentFilter } = this.props
+    const { mySchedule, date } = this.state
+    const page = 'scoreboard'
 
     let filteredScoreData = mySchedule
     R.values(contentFilter[page]).forEach(filter => {
       filteredScoreData = Filterer(mySchedule, filter)
     })
-    const sports = R.values(sportLeagues).sort((x,y) => x.order > y.order).map(x => x.sport_id)
+    const sports = R.values(sportLeagues).sort((x, y) => x.order > y.order).map(x => x.sport_id)
     sports.unshift('All')
 
-    const filter = {
-      type:'tab',
-      displayType:'sportsIcon',
-      values:sports,
-      column:'sport_id',
-      defaultTab:0,
-      tabStyles:{backgroundColor:'#392007', color:'white',
-        selectedBackgroundColor:'#392007', 
-        selectedColor:'#EBAB38'}
+    const sportFilter = {
+      type: 'tab',
+      displayType: 'sportsIcon',
+      values: sports,
+      column: 'sport_id',
+      defaultTab: 0,
+      tabStyles: {
+        backgroundColor: '#392007', color: 'white',
+        selectedBackgroundColor: '#392007',
+        selectedColor: '#EBAB38'
+      }
     }
+
+    const teamFilter = {
+      type: 'tab',
+      values: this.props.activeLeague.owners.map(x => x.owner_name).sort((a, b) => a > b),
+      column: 'owner_name',
+      defaultTab: 0,
+      tabStyles: {
+        backgroundColor: '#e3dac9',
+        color: '#48311A',
+        selectedBackgroundColor: 'white',
+        selectedColor: '#229246',
+        fontSize: 12
+      }
+    }
+
+
     return (
       <div>
-        <Title color='white' backgroundColor='#EBAB38' title={'Scoreboard'}/>
-        <FilterCreator filters={[filter]} page={page}/>
-        <ScoreboardBody scoreData={filteredScoreData} date={date} onUpdateDate={this.onUpdateDate}/>
+        <Title color='white' backgroundColor='#EBAB38' title={'Scoreboard'} />
+        <FilterCreator filters={[sportFilter]} page={page} />
+        <FilterCreator filters={[teamFilter]} page={page} />
+        <ScoreboardBody scoreData={filteredScoreData} date={date} onUpdateDate={this.onUpdateDate} />
       </div>
 
     )
