35054b11921763bc7994e219a391245c94b2a557
Unknown <matherts@gmail.com>
2018-09-04 22:33:39 -0500

    fixes to create League Email template

--- server/email-templates/createleague.js
+++ server/email-templates/createleague.js
@@ -2,7 +2,7 @@ const path = require('path')
 const dev = process.env.NODE_ENV !== 'production'
 const header = path.join(__dirname, '/Derby_Email_headline_A-New-Way-to-Play_2.jpg')
 const footer1 = path.join(__dirname, '/Derby_Email_sports_icons_2x.png')
-const footer2 = path.join(__dirname, '/Derby_Email_footer_text_2x.png')
+const footer2 = path.join(__dirname, '/Derby_Email_footer_newest.png')
 
 const createLeagueSubject = (user) => `You've Created a League!`
 
@@ -14,23 +14,23 @@ const createLeagueBody = (user) => {
         <td align="center">
           <img src="cid:Derby_Email_headline_A-New-Way-to-Play_2.jpg" width="600" height="181">
           <div style="width: 600px; font-family: 'Roboto'; font-size: 1.2em; margin: 30px; color: 'black';">
-            <h3 style="text-align: left;">Dear ${user.first_name}!</h3>
-            <p style="text-align: left;">You've just created a Derby Fantasy Wins League</p>
+            <h3 style="text-align: left;">Congratulations, ${user.first_name}!</h3>
+            <p style="text-align: left;">You have just created a new league at Derby Fantasy Wins League.</p>
             <p style="text-align: left;">Now's your chance to get the race started by inviting others to join your league.</p>
             <p style="text-align: left;">You can invite others to sign up on <a href="https://www.derby-fwl.com">Derby FWL</a>
             using your league name and password below: </p>
             <p style="text-align: left;">
-              League Name: <b>${user.league_name}</b><br />
-              Password: <br>${user.league_password}</b>
+              League Name: <b>${user.league_name}</b><br>
+              Password: <b>${user.league_password}</b>
             </p>
-            <p style="text-align: left;">You can also change your league settings, draft time, and invite managers on Derby Fantasy Wins League from our home page.</p>
+            <p style="text-align: left;">You can also change your league settings and draft time on Derby Fantasy Wins League from the ${user.league_name} Commish Tools page.</p>
             <p style="text-align: left;">As a reminder, your league is currently set to draft on ${user.draft_day} at ${user.draft_time}.</p>
           </div>
           <p>
             <img src="cid:Derby_Email_sports_icons_2x.png" width="600" height="66">
           </p>
           <p>
-            <img src="cid:Derby_Email_footer_text_2x.png" width="600" height="50">
+            <img src="cid:Derby_Email_footer_newest.png" width="600" height="83">
           </p>
         </td>
       </tr>
