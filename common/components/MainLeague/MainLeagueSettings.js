32a6f6899778e5836db75f01468596b9e4d0ccfb
Yoni Silverman <yoni.silverman@mcmaster.com>
2018-09-22 16:26:40 -0500

    cant update after draft is over

--- common/components/MainLeague/MainLeagueSettings.js
+++ common/components/MainLeague/MainLeagueSettings.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
+import C from '../../constants'
 import Title from '../Navigation/Title'
 import LeagueSettings from '../LeagueSettings/LeagueSettings'
+import { Typography } from '@material-ui/core';
 
 class MainLeagueSettings extends React.Component {
 
@@ -17,7 +18,22 @@ class MainLeagueSettings extends React.Component {
           title="Commish Tools"
           subheading={activeLeague.league_name}
         />
-        <LeagueSettings />
+        {
+          this.props.activeLeague.draftInfo.mode === C.DRAFT_STATE.PRE ?
+            <LeagueSettings /> :
+            <div style={{height:500}}>
+              <div style={{
+                marginTop:100,display:'flex', alignItems:'center', flexDirection:'column'}}>
+                {this.props.activeLeague.draftInfo.mode === C.DRAFT_STATE.POST ?
+                  <Typography style={{ textAlign:'center'}}  variant='display1'>Current season is underway.</Typography> :
+                  <Typography style={{ textAlign:'center'}}  variant='display1'>Draft Is Live.</Typography>
+                }
+                <Typography style={{marginTop:50, textAlign:'center'}} variant='display1'>Changes are no longer allowed.</Typography> 
+              </div>
+            </div>
+
+        }
+        
       </div>
     )
   }
