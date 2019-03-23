c259d590d62c844dd96112a35c9147e48bea3ed0
Yoni Silverman <yoni.silverman@mcmaster.com>
2019-03-23 10:04:53 -0500

    add migration for prod

---
+++ server/db/migrations/prod/20190323100245_fantasy-invites.js
@@ -0,0 +1,21 @@
+const STATUS_ENUM = ['invited_to_signup', 'not_invited', 'invited']
+
+
+exports.up = function(knex, Promise) {
+  return knex.schema.withSchema('fantasy').createTable('invites', (t) => {
+    t.increments()
+    t.string('invite_id').unique().notNullable()
+    t.string('league_id').notNullable()
+    t.string('from_id').notNullable()
+    t.string('email').notNullable()
+    t.enum('status', STATUS_ENUM).notNullable()
+    t.timestamps()
+    t.index('invite_id')
+    t.index('league_id')
+    t.unique(['league_id', 'from_id', 'email'])
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.withSchema('fantasy').dropTable('invites')
+}
